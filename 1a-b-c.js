8ee5e93e936137974e8d0cd96ad8345af2852057
euge <meugenia.moreno@gmail.com>
2020-12-04 20:33:09 -0300

    ejercicio 1 - a,b y c

---
+++ 1a-b-c.js
@@ -0,0 +1,12 @@
+var precio1 = 5
+var precio2 = 3
+var precio3 = 2
+
+var suma = precio1+precio2+precio3;
+alert('la suma es: '+ suma);
+
+var promedio = suma/3;
+console.log(promedio)
+
+var precioConIVA = suma * 1.21
+console.log(precioConIVA)
